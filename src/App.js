86363fdc4a74d706546eb48d7fd971741e88f7dd
janik.buecher-cic@ibm.com <janik.buecher-cic@ibm.com>
2021-11-18 16:19:34 +0100

    pipeline trigger

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 
 import Notes from "./pages/Notes/NotesPage";
 
-
 import './App.css';
 export default function App() {
   return (
